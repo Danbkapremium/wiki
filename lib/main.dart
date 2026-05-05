d009650713fd1d4e9d7431937452e0b6ce64abef
danya <daniln.08@mail.ru>
2026-05-05 12:05:52 +0500

    xyi

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,46 @@ void main() {
   runApp(const MainApp());
 }
 
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      "en.wikipedia.org",
+      "/api/rest_v1/page/random/summary",
+    );
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw const HttpException("Failed to recieve page summary");
+    }
+    return Summary.fromJson(jsonDecode(response.body) as Map<String, Object?>);
+  }
+}
+
+class ArticleViewModel extends ChangeNotifier {
+  final ArticleModel model;
+  Summary? summary;
+  Exception? error;
+  bool isLoading = false;
+  ArticleViewModel(this.model) {
+    fetchArticle();
+  }
+
+  Future<void> fetchArticle() async {
+    try {
+      isLoading = true;
+      notifyListeners();
+
+      summary = await model.getRandomArticleSummary();
+      error = null;
+    } on HttpException catch (e) {
+      error = e;
+      summary = null;
+    }
+    isLoading = false;
+    notifyListeners();
+  }
+}
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -15,30 +55,59 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text('Loading...')),
-        appBar: AppBar(
-          title: Align(
-            alignment: Alignment.centerLeft,
-            child: Text("Wikipedia"),
-          ),
-        ),
+        appBar: AppBar(title: Text("Wikipedia")),
+        body: Center(child: Text('loading...')),
       ),
     );
   }
 }
 
-class ArticleModel {
-  Future<Summary> getRandomPageSummary() async {
-    final uri = Uri.https(
-      "enwikipedia.org",
-      "/api/rest_vl/page/random/summary",
+class ArticleView extends StatefulWidget {
+  const ArticleView({super.key});
+
+  @override
+  State<ArticleView> createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  final ArticleViewModel viewModel = ArticleViewModel(ArticleModel());
+  @override
+  void initState() {
+    super.initState();
+    viewModel.fetchArticle();
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  final Summary summary;
+  final VoidCallback nextArticle;
+  ArticleWidget({super.key, required this.summary,required this.nextArticle});
+
+  @override
+  Widget build(BuildContext context) {
+     return SingleChildScrollView(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+
+          SizedBox(height:10),
+
+          Text(summary.titles.normalized),
+
+          SizedBox(height:10),
+
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              style: Theme.of(context).textTheme.displaySmall,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(height:10),
+
+          Text(summary.extract),
+        ],
+      ),
     );
-
-    final response = await get(uri);
-
-    if (response.statusCode != 200) {
-      throw const HttpException("Failed to recieve page summary");
-    }
-    return Summary.fromJson(jsonDecode(response.body) as Map<String, Object>);
   }
 }
